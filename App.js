15fd5897095c64344cfd00bdc28bd738fe582d1c
Kdorina <kovacsdorina2@gmail.com>
2023-04-05 17:18:48 +0200

    szamitás megírása

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const[long , setLong]=useState('');
+  const[perimeter, setPerimeter]=useState('');
+  const[result , setResult]=useState('');
+
+  function calcWeight(){
+    let result = (long*long*perimeter)/11877;
+    setResult(result);
+  }
   return (
     <View style={styles.container}>
       <Text>Ló súly számítás</Text>
